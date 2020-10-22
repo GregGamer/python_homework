c637d5d0d53d6ca106d4213d67e70bae865db700
DESKTOP-2FSB12N\Gregor <gregor.wagner@sz-ybbs.ac.at>
2020-10-22 11:52:53 +0200

    finish U3Bsp4

--- Übungsblatt3/U3Bsp4.py
+++ Übungsblatt3/U3Bsp4.py
@@ -3,3 +3,41 @@ Gregor Wagner
 U3Bsp4.py - 
 Gregor Wagner, 52005240
 """
+import string
+import random
+
+def main() :
+    passwordLen = 12
+    passwordUp = 2
+    passwordNum = 2
+    passwordSign = 2
+    i = 0
+    password = ""
+
+    while len(password) < passwordLen :
+        for i in range(2) :
+            if len(password) < passwordLen :
+                password += random.choice(string.ascii_lowercase)
+            
+        for i in range(passwordUp) :
+            if len(password) < passwordLen :
+                password += random.choice(string.ascii_uppercase)
+        
+        for i in range(passwordNum) :
+            if len(password) < passwordLen :
+                password += random.choice(string.digits)
+        
+        for i in range(passwordSign) :
+            if len(password) < passwordLen :
+                password += random.choice(string.punctuation)
+    
+    l = list(password)
+    random.shuffle(l)
+    password = ''.join(l)
+
+    print(f"Passwort: {password} \nLänge: {passwordLen}")
+
+
+
+if __name__ == "__main__":
+    main()

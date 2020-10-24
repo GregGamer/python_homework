a1ea164621c657c31c8daa18e8632c81d284e21c
DESKTOP-2FSB12N\Gregor <gregor.wagner@sz-ybbs.ac.at>
2020-10-24 17:05:49 +0200

    finish U4Bsp2

--- Übungsblatt4/U4Bsp2.py
+++ Übungsblatt4/U4Bsp2.py
@@ -3,3 +3,23 @@ Gregor Wagner
 U4Bsp2.py - 
 Gregor Wagner, 52005240
 """
+from collections import Counter
+
+def main() :
+    data = []
+    
+    f = open("einkaufsliste.txt", mode="r", encoding="utf-8")
+    
+    for x in f:
+        x = x.replace("\n", "")
+        print(x)
+        data.append(x)
+
+    f.close()
+
+    for key, value in Counter(data).items() :
+        print(f"{value}x -> {key}")
+
+
+if __name__ == "__main__":
+    main()

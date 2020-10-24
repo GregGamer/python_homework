7407f2b31b7a04d258bcbe978d4789a7309509e8
DESKTOP-2FSB12N\Gregor <gregor.wagner@sz-ybbs.ac.at>
2020-10-24 20:59:20 +0200

    update U3Bsp3

--- Übungsblatt3/U3Bsp3.py
+++ Übungsblatt3/U3Bsp3.py
@@ -7,32 +7,19 @@ import random
 import time
 
 def getRandomSample() :
-    return random.sample(range(46),6)
+    return random.sample(range(1,46),6)
 
-def main() :
-    startTime = time.time()
-    startNumbers = getRandomSample()
-    # startNumbers = [41,34,15,3,10,44]
-    equal = 0
-    counter = 0
-    randomNumbers = []
-    # money = 1000
-    # price = [0,0,3,10,50,4000,800000]
+startTime = time.time()
+startNumbers = getRandomSample()
+equal = 0
+counter = 0
+randomNumbers = []
 
-    while equal != 4 or startNumbers == randomNumbers:
-        randomNumbers = getRandomSample()
-        # money -= 1
-        counter += 1
-        if startNumbers != randomNumbers :
-            equal = len(set(startNumbers) & set(randomNumbers))
-            # print(f"{startNumbers} != {randomNumbers} \t => equal = {equal} => {counter}    ")
-        else :
-            print("gleich")
-        endTime = time.time()
-        # money += price[equal]
-    takenTime = round(endTime-startTime, 2)
-    print(f"Ihre Gewinnerzahlen: {randomNumbers} \t Versuche: {counter} \nTime: {takenTime}s")
-        # print(f"{counter} \t money = {money}")
+while equal < 5:
+    randomNumbers = getRandomSample()
+    counter += 1
+    equal = len(set(startNumbers) & set(randomNumbers))
+    endTime = time.time()
 
-if __name__ == "__main__":
-    main()
+takenTime = round(endTime-startTime, 2)
+print(f"Ihre Gewinnerzahlen: {startNumbers} = {randomNumbers} \t Versuche: {counter} \nTime: {takenTime}s")

8a3baa98a467a09b3fc870350377816f0942417b
DESKTOP-2FSB12N\Gregor <gregor.wagner@sz-ybbs.ac.at>
2020-10-28 12:18:02 +0100

    update zum optionalen Programm

--- Übungsblatt4/U4Bsp_optional.py
+++ Übungsblatt4/U4Bsp_optional.py
@@ -2,9 +2,17 @@
 Gregor Wagner
 U4Bsp_optional.py - Consolen Programm - Bug/Issue Tracker
 Gregor Wagner, 52005240
+
+Upate zum "alten" Programm:
+    hinzugefügt:
+        command: save file          //es ist jetzt möglich alle Daten die erstellt worden sind als file zu speichern
+        command: load file          //es ist jetzt möglich die gespeicherten Daten zu laden und damit weiterzuarbeiten, [gibt noch kleine bugs]
+        command: list all events     //weil "list events" nur die offenen bugs angezeigt hat
+    Error handling bei falschen inputs
 """
-# TODO - color Coding, alle Inputs grün, IDs rot, verfügbare Befehle blau, offene Events gleb und geschlossene schwarz, bei den Events ein color Coding je nach prio
+
 import time
+import json
 
 class Event():
     eid = 0
@@ -32,6 +40,9 @@ class Bug(Event):
     def setGitHash(self, gitHash):
         self.gitHash = gitHash
 
+    def jsonencode(self):
+        return {"reporter": self.reporter.pid, "title": self.title, "description": self.description, "priority": self.priority, "isOpen": self.isOpen, "gitHash": self.gitHash}
+
 class Incident(Event):
     def __init__(self, reporter, title, description, priority, cve):
         super().__init__(reporter, title, description, priority)
@@ -43,6 +54,9 @@ class Incident(Event):
     def setCve(self, cve) : 
         self.cve = cve
 
+    def jsonencode(self):
+        return {"reporter": self.reporter.pid, "title": self.title, "description": self.description, "priority": self.priority, "isOpen": self.isOpen, "cve": self.cve}
+
 class Person():
     pid = 0
     def __init__(self, name, email):
@@ -50,6 +64,9 @@ class Person():
         Person.pid += 1
         self.name = name
         self.email = email
+
+    def jsonencode(self):
+        return {"name": self.name, "email":self.email}
 
     def __str__(self):
         return f"\033[0;31;40m{self.pid}\033[0;33;40m, Name: {self.name}, Email: {self.email}\033[0;37;40m"
@@ -88,7 +105,10 @@ def newBug(eventListe, personenListe):
         print(p)
     
     #TODO - try catch über die inputs und abfrage ob die eingegebene pid auch existiert
-    pid = int(input("--> "))
+    try:
+        pid = int(input("--> "))
+    except ValueError:
+        print("Fehler")
 
     print(f"Reporter: {personenListe[pid].name}")
     titel = input("Titel --> ")
@@ -130,6 +150,12 @@ def listEvents(eventListe, personenListe):
         if e.isOpen == True :
             print(e)
 
+def listAllEvents(eventListe, personenListe):
+    print("Events")
+    print("------")
+    for e in eventListe : 
+        print(e)
+
 def closeEvent(eventListe, personenListe):
     for e in eventListe :
         if e.isOpen == True :
@@ -142,12 +168,46 @@ def closeEvent(eventListe, personenListe):
 
     print("Event wurde geschlossen!")
 
+def saveFile(eventListe, personenListe):
+    filename = input("Filename --> ") + ".json"
+    f = open(filename, mode="w", encoding="utf-8")
+
+    jsonList = {"personenListe": [], "eventListe": []}
+    for p in personenListe :
+        print(p.jsonencode())
+        jsonList["personenListe"].append(p.jsonencode())
+
+    for e in eventListe :
+        print(e.jsonencode())
+        jsonList["eventListe"].append(e.jsonencode())
+
+    print(jsonList)
+    f.write(json.dumps(jsonList))
+
+    f.close()
+
+
+def loadFile(eventListe, personenListe):
+    filename = input("Filename --> ") + ".json"
+    f = open(filename, mode="r", encoding="utf-8")
+
+    jsonList = json.load(f)
+    for p in jsonList["personenListe"] : 
+        personenListe.append(Person(p['name'], p['email']))
+    i = 0
+    for e in jsonList['eventListe'] :
+        if 'cve' in e :
+            eventListe.append(Incident(personenListe[e['reporter']], e['title'], e['description'], e['priority'], e['cve']))
+        elif 'gitHash' in e :
+            eventListe.append(Bug(personenListe[e['reporter']], e['title'], e['description'], e['priority'], e['gitHash']))
+        if e['isOpen'] == False :
+            eventListe[i].isOpen = False
+        i += 1
+    
+
 
 def listCommands(commands):
-    # TODO - verbesserte Art commands hinzuzufügen, mybe via dictionary
-    
     commandString = " | ".join(list(commands.keys()))
-
     print(f"\033[1;34;40mBefehle: exit | {commandString}\033[0;37;40m")
 
 
@@ -157,11 +217,14 @@ def main() :
     personenListe = []
     eventListe = []
     
-    commands = {"new person"    : newPerson, 
+    commands = {"save file"     : saveFile,
+                "load file"     : loadFile,
+                "new person"    : newPerson, 
                 "list persons"  : listPersons, 
                 "new bug"       : newBug, 
                 "new incident"  : newIncident, 
                 "list events"   : listEvents, 
+                "list all events": listAllEvents,
                 "close events"  : closeEvent}
 
     while (consoleLog := input("--> ")) != "exit" :

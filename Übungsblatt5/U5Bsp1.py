557a10d564612dca8c457cb052248a91ba103e85
DESKTOP-2FSB12N\Gregor <gregor.wagner@sz-ybbs.ac.at>
2020-10-29 11:49:35 +0100

    finish U5Bsp2

--- Übungsblatt5/U5Bsp1.py
+++ Übungsblatt5/U5Bsp1.py
@@ -1,5 +1,86 @@
 """
 Gregor Wagner
-U5Bsp1.py - 
+U5Bsp1.py - small tweets, Big Data analysis
 Gregor Wagner, 52005240
 """
+import json
+import re
+from collections import Counter
+class Tweet():
+    def __init__(self, time, message, user):
+        self.time = time
+        self.message = message
+        self.user = user.split("/")[-1]
+
+def readTweets(tweets):
+    f = open("twitter_small.json", mode="r", encoding="utf=8")
+    twitter_data = json.load(f)
+    f.close
+
+    for t in twitter_data['tweets'] :
+        tweets.append(Tweet(t['time'], t['message'].lower(), t['user']))
+
+
+def listCommands(commands):
+    print("Usage: allowed keywords are: ")
+    for key, value in commands.items() :
+        print(f"\t{key} {value.__name__}")
+    print("\texit")
+    print("help for Usage")
+
+def tweet_count(tweets):
+    print(f"Anzahl der Tweets: {len(tweets)}")
+
+def top_hashtags(tweets):
+    pattern = '#\w*'
+    hashtags = []
+    for t in tweets :
+        if "#" in t.message :
+            hashtags.extend(re.findall(pattern, t.message))
+    print(f"Top 5 Hashtags:  {Counter(hashtags).most_common(5)}")
+
+def top_users(tweets):
+    users = []
+    for t in tweets :
+        users.append(t.user)
+    print(f"Top 5 Users: {Counter(users).most_common(5)}")
+
+def top_tweet_day(tweets):
+    tweetsInDays = []
+    for t in tweets :
+        tweetsInDays.append(t.time.split(" ")[0])
+    print(f"Top Tage an denen gepostet wurde: {Counter(tweetsInDays).most_common(5)}")
+
+
+def top_hashtag_user(tweets):
+    pass
+
+def main():
+    tweets = []
+
+    commands = {"1" : tweet_count,
+                "2" : top_hashtags,
+                "3" : top_users, 
+                "4" : top_tweet_day, 
+                "5" : top_hashtag_user}
+
+
+    listCommands(commands)
+
+    readTweets(tweets)
+    print("\nTweets successfully imported\n")
+
+    print("------------------------")
+    while (consoleLog := input("Keyword --> ")) != "exit" :
+        command = consoleLog.lower()
+        for key, value in commands.items() :
+            if command == key.lower() :
+                value(tweets)
+                print("------------------------")
+            
+        if command not in commands :
+            listCommands(commands)
+
+
+if __name__ == "__main__":
+    main()
--- Übungsblatt5/U5Bsp2.py
+++ Übungsblatt5/U5Bsp2.py
@@ -1,5 +1,70 @@
 """
 Gregor Wagner
-U5Bsp2.py - 
+U5Bsp2.py - Hangman
 Gregor Wagner, 52005240
 """
+import sys
+import random
+
+def usage():
+    print("python U5Bsp.py <min-word-length> <max-word-length> <just Filename>")
+
+def getRandomWord() :
+    if len(sys.argv) == 4 :
+        wordmin = int(sys.argv[1])
+        wordmax = int(sys.argv[2])
+        filename = sys.argv[3] + ".txt"
+    else :
+        usage()
+        quit()
+
+    f = open(filename, mode="r", encoding="utf-8")
+
+    wordlist = []
+    for w in f :
+        w = w.replace("\n", "")
+        if wordmin <= len(w) <= wordmax :
+            wordlist.append(w)
+    
+    f.close()
+
+    randomWord = random.choice(wordlist)
+    return randomWord.upper()
+
+def main() :
+    word = str(getRandomWord())
+    usedLetters = list()
+    solvedWord = []
+    fails = 0
+
+    for w in word :
+        solvedWord.append("-")
+
+    print("Welcome to my Hangman Game")
+    while True :
+        print(" ".join(solvedWord))
+        guess = input("Guess your letter: ").upper()
+        usedLetters.append(guess)
+        if guess in word:
+            print("HIT")
+            for i in range(len(word)) :
+                if guess == word[i] :
+                    solvedWord[i] = word[i]
+        else:
+            if guess not in usedLetters :
+                fails += 1
+            print(f"FAIL: {fails}")
+            if fails >= 10 :
+                print("Nicht geschafft")
+                quit()
+        print(f"used letters: {sorted(list(set(usedLetters)))}")
+
+        if "-" not in solvedWord :
+            solve = "".join(solvedWord)
+            print(f"solved\nWhat is the word: {word}\nGood job! You figured that the word is {solve}")
+            quit()
+
+
+
+if __name__ == "__main__":
+    main()

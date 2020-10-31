3b62c9c74408e437871411c61cb1e93770873b23
DESKTOP-2FSB12N\Gregor <gregor.wagner@sz-ybbs.ac.at>
2020-10-31 22:40:21 +0100

    finish with helper list of the possible cities/countries

--- Übungsblatt5/U5Bsp2.py
+++ Übungsblatt5/U5Bsp2.py
@@ -3,15 +3,14 @@ Gregor Wagner
 U5Bsp2.py - Hangman
 Gregor Wagner, 52005240
 """
-import sys
-import random
+import sys, random, re
 
-DEBUG = False
+DEBUG = True
 
 def usage():
     print("python U5Bsp.py <min-word-length> <max-word-length> <just Filename>")
 
-def getRandomWord() :
+def getRandomWord(wordlist) :
     if len(sys.argv) == 4 :
         wordmin = int(sys.argv[1])
         wordmax = int(sys.argv[2])
@@ -26,7 +25,6 @@ def getRandomWord() :
 
     f = open(filename, mode="r", encoding="utf-8")
 
-    wordlist = []
     for w in f :
         w = w.replace("\n", "")
         if wordmin <= len(w) <= wordmax :
@@ -37,9 +35,31 @@ def getRandomWord() :
     randomWord = random.choice(wordlist)
     return randomWord.upper()
 
+
+def possibilities(solvedWord, wordlist, wrongLetters) :
+    word = "".join(solvedWord)
+    if wrongLetters :
+        wrong = "[^"+ "".join(wrongLetters) + "]"
+        pattern = "^" + word.replace("-",wrong) + "$"
+    else :
+        pattern = "^" + word.replace("-","\w") + "$"
+    
+    erg = []
+    for w in wordlist :
+        if found := re.findall(pattern, w.upper()) :
+            erg.append(found[0])
+
+    return erg
+
+
+
 def main() :
-    word = str(getRandomWord())
-    usedLetters = list()
+    mitHilfe = True
+
+    wordlist = []
+    word = str(getRandomWord(wordlist))
+    rightLetters = list()
+    wrongLetters = list()
     solvedWord = []
     for w in word :
         solvedWord.append("-")
@@ -48,14 +68,15 @@ def main() :
     print("Welcome to my Hangman Game")
     while True :
         # show used Letters
-        print(f"used letters: {sorted(list(set(usedLetters)))}")
+        print(f"right letters: {sorted(list(set(rightLetters + wrongLetters)))}")
         # show solved Part of the word: ---A-A-
         print(" ".join(solvedWord))
+
 
         # input
         guess = input("Guess your letter: ").upper()
         
-        if guess in "".join(usedLetters) :
+        if guess in "".join(rightLetters) :
             print("Ooops! Already guessed that letter - try again")
             continue
         
@@ -63,7 +84,7 @@ def main() :
             if guess == "SOLVE" :
                 solve = input("What is the word: ").upper()
                 if solve == word :
-                    print(f"Good job! You figured that the word is {solve.upper()}")
+                    print(f"Good job! You figured that the word is {word}")
                     break
                 else :
                     print(f"Sorry, that was not the word we are searching for.")
@@ -72,10 +93,17 @@ def main() :
             if guess == "QUIT" :
                 print(f"The word we were searching for was: {word}")
                 break
-        elif len(guess) == 1 :
-            # print HIT or FAIL
-            print("HIT" * (guess in word) + "FAIL" * (guess not in word))
-        
+            if guess == word :
+                print(f"Good job! You figured that the word is {word}")
+                break
+            else :
+                fails += 1
+                if mitHilfe :
+                    # shows all posibilities based on the right letters
+                    for p in possibilities(solvedWord, wordlist, wrongLetters) :
+                        print(p)
+                print("FAIL")
+        elif len(guess) == 1 :        
             if guess in word :
                 for i in range(len(word)) :
                     if guess == word[i] :
@@ -83,16 +111,32 @@ def main() :
                 if "".join(solvedWord) == word :
                     print(f"Congratulations, you found out that the word is {word}")
                     break
+
+                if mitHilfe :
+                    # shows all posibilities based on the right letters
+                    for p in possibilities(solvedWord, wordlist, wrongLetters) :
+                        print(p)
+
+                print("HIT")
+                rightLetters.append(guess)
             else :
                 fails += 1
+                wrongLetters.append(guess)
+
+                if mitHilfe :
+                    # shows all posibilities based on the right letters
+                    for p in possibilities(solvedWord, wordlist, wrongLetters) :
+                        print(p)
+
+                print("FAIL")
                 print(f"Fails: {fails}")
+
 
             if fails >= 10 :
                 print(f"You had too many tries, maybe u get it next time.")
                 print(f"The word we were searching for was: {word}")
                 break
 
-            usedLetters.append(guess)
 
 if __name__ == "__main__":
     main()
